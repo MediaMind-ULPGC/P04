b53ccb4880d5957a473582bd4f582099fa6b261e
Susana Suárez <susanasuarezmendoza26@gmail.com>
2024-10-23 12:17:58 +0100

    escalas

--- identifiers/scales/piano_scale.py
+++ identifiers/scales/piano_scale.py
@@ -9,7 +9,7 @@ class PianoScaleIdentifier:
 
     __piano_identifier = PianoIdentifier()
 
-    def identify_scale_from_audio(self, audio_file, window_size=4096, overlap=2048, tolerance=10, num_frequencies=3):
+    def identify_scale_from_audio(self, audio_file, window_size=4096, overlap=2048, tolerance=10, num_frequencies=3, timestep=0.1):
         """
         Identificar la escala de un archivo de audio dividiéndolo en ventanas temporales y
         utilizando la clase DominantFrequency para identificar las frecuencias dominantes en cada ventana.
@@ -19,44 +19,39 @@ class PianoScaleIdentifier:
         if len(data.shape) > 1:
             data = np.mean(data, axis=1)
 
+        window_size_seconds = timestep
+        window_size = int(sr * window_size_seconds)
+        timestep_samples = int(timestep * sr)
+
         identified_notes = []
+        identified_octaves = []
         dominant_frequencies = []
         time_stamps = []
 
-        # Procesar el audio por ventanas
-        for start in range(0, len(data) - window_size, window_size - overlap):
+        # Recorremos los datos con un paso de tiempo fijo
+        for start in range(0, len(data) - window_size, timestep_samples):
             window_data = data[start:start + window_size]
-            
-            # Calculamos el tiempo correspondiente al centro de la ventana actual
+
             time_stamp = (start + window_size / 2) / sr
-            # Obtener las frecuencias dominantes de la ventana
+
             fourier, freqs = DominantFrequency._get_frequencies(sr, window_data)
             peaks, _ = find_peaks(fourier, height=0)
             
-            # Ordenar las frecuencias dominantes por magnitud
-            sorted_peaks = np.argsort(fourier[peaks])[::-1]
-            
-            # Capturar las principales frecuencias dominantes
-            window_frequencies = []
-            window_notes = []
-            for peak_index in sorted_peaks[:num_frequencies]:
-                dominant_frequency = freqs[peaks][peak_index]
+            if peaks.size > 0:
+                sorted_peaks = np.argsort(fourier[peaks])[::-1]
+                
+                # Capturar solo la frecuencia dominante (la más alta)
+                dominant_frequency = freqs[peaks][sorted_peaks[0]]
 
-                # Filtrar frecuencias no relevantes
                 if 20 < dominant_frequency < 4000:
-                    # Identificar la nota correspondiente
-                    _, note = self.__piano_identifier.identify_note_from_frequency(dominant_frequency, tolerance)
+                    octava, note = self.__piano_identifier.identify_note_from_frequency(dominant_frequency, tolerance)
                     if note:
-                        window_notes.append(note[5:])
-                        window_frequencies.append(dominant_frequency)
+                        identified_notes.append([note[5:]])
+                        identified_octaves.append([octava])
+                        dominant_frequencies.append([dominant_frequency])
+                        time_stamps.append(time_stamp)
 
-            # Solo añadimos si identificamos al menos una frecuencia
-            if window_notes and window_frequencies:
-                identified_notes.append(window_notes)
-                dominant_frequencies.append(window_frequencies)
-                time_stamps.append(time_stamp)
-
-        return identified_notes, dominant_frequencies, time_stamps
+        return identified_notes, identified_octaves, dominant_frequencies, time_stamps
 
     def plot_scale_over_time(self, notes, frequencies, time_stamps):
         # Comprobar si el tamaño de time_stamps y frequencies es el mismo
